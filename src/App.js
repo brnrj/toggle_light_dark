ae501870779d6ee709121a28284449b1c0ec8535
Bruno Martins <bruno.martins217@gmail.com>
2021-04-10 22:29:51 -0300

    toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import Toggle from './components/Toggle';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={toggleTheme} type="button">
+          Toggle theme
+        </Toggle>
+        <h1>It`s a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+        <footer>
+          <span>Credits:</span>
+          {' '}
+          <small>
+            <b>Sun</b> icon made by{' '}
+            <a href="https://www.flaticon.com/authors/smalllikeart">
+              smalllikeart
+            </a>{' '}
+            from <a href="https://www.flaticon.com">www.flaticon.com</a>
+          </small>
+          {' '}
+          <small>
+            <b>Moon</b> icon made by{' '}
+            <a href="https://www.freepik.com/home">Freepik</a> from{' '}
+            <a href="https://www.flaticon.com">www.flaticon.com</a>
+          </small>
+        </footer>
+      </>
+    </ThemeProvider>
   );
 }
 
